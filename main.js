11fd8e541ce11efbe33d35d0fbd28eb8a82c5f91
ElisaTorelli <elisa.torelli1@gmail.com>
2022-05-25 14:49:29 +0200

    responsive navbar

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ let hamburger = document.querySelector(".hamburger-menu");
 let navBarMenu = document.querySelector(".nav-menu");
 
 // ADD EVENT LISTENER WHEN CLICK ON BARS
-hamburger.addEventListener("click", hamburgerMenu);
-
 function hamburgerMenu() {
     hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    navBarMenu.classList.toggle("nav-menu-active");
 }
+
+hamburger.addEventListener("click", hamburgerMenu);
+
 
 
 // TO CLOSE HAMBURGER MENU WHEN CLICK ON X BAR
-let navBarLink = documen.querySelectorAll('.nav-link');
-
-navLink.forEach(linkBarElement => linkBarElement.addEventListener("click", closeMenu));
+let navBarLink = document.querySelectorAll('.nav-link');
 
 function closeMenu() {
     hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+    navBarMenu.classList.remove("nav-menu-active");
 }
+
+navBarLink.forEach(linkBarElement => linkBarElement.addEventListener("click", closeMenu));
